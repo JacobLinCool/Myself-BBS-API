780689f2d53506b0d2ad0038ed5796f681721f15
JacobLinCool <jacoblincool@gmail.com>
2021-06-30 17:47:00 +0800

    Update new.js

--- src/new.js
+++ src/new.js
@@ -7,10 +7,10 @@ async function get_new_animes() {
     let page1 = await fetch("https://myself-bbs.com/forum-133-1.html?forumdefstyle=yes").then((r) => r.text());
 
     let total_regexp = new RegExp(`<span title="共 (\\d) 頁">`, "g");
-    let total = +[...page1.matchAll(total_regexp)][0][1];
+    let total = +[...page1.matchAll(total_regexp)][0][1] || 1;
 
     let item_regexp = new RegExp(
-        `<em>\\[<a href="forum.php\\?mod=forumdisplay&fid=133&amp;filter=typeid&amp;typeid=(\\d+?)">([^]+?)<\\/a>]<\\/em> <a href="thread-(\\d+?)-1-1.html" onclick="atarget\\(this\\)" class="xst" >([^]+?)<\\/a>`,
+        `<em>\\[<a href="forum.php\\?mod=forumdisplay&fid=133&amp;filter=typeid&amp;typeid=(\\d+?)">([^]+?)<\\/a>]<\\/em> <a href="thread-(\\d+?)-1-\\d.html" onclick="atarget\\(this\\)" class="xst" >([^]+?)<\\/a>`,
         "g"
     );
 
@@ -25,6 +25,7 @@ async function get_new_animes() {
 
     for (let i = 2; i <= total; i++) {
         let page = await fetch(`https://myself-bbs.com/forum-133-${i}.html?forumdefstyle=yes`).then((r) => r.text());
+
         [...page.matchAll(item_regexp)].map((x) => {
             list.push({
                 id: x[3].trim(),
